9e08499dbecb90fe4ecd62fce4b01b12bf8de395
GitGuruRI <raisulislam0341@gmail.com>
2024-07-29 12:41:05 +0530

    Basic Data Types

---
+++ Sheet-1 (Data type -Conditions)/Problem-B.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // declare basic data types variable
+    int num1;
+    long long num2;
+    char letter;
+    float fnum;
+    double dnum;
+
+    // get user input
+    cin >> num1 >> num2 >> letter >> fnum >> dnum;
+
+    // print data type variable value
+    cout << num1 << endl << num2 << endl << letter << endl << fnum << endl << dnum;
+    
+    return 0;
+}
