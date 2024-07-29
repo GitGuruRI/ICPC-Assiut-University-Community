da39198c6a68651269d06457d6bacad97ab9f0f5
GitGuruRI <raisulislam0341@gmail.com>
2024-07-29 12:49:32 +0530

    Area of a Circle

---
+++ Sheet-1 (Data type -Conditions)/Problem-E.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double r, pi = 3.141592653;
+    cin >> r;
+
+    // circle area
+    cout << fixed << setprecision(9);
+    cout << pi * r * r;
+
+    return 0;
+}
