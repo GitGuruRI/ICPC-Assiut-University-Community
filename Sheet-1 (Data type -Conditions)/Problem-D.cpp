c7331d0fce4d605d39d0cb3db37de2379278d5fd
GitGuruRI <raisulislam0341@gmail.com>
2024-07-29 12:46:21 +0530

    Difference

---
+++ Sheet-1 (Data type -Conditions)/Problem-D.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    // difference
+    cout << "Difference = " << (a * b) - (c * d);
+
+    return 0;
+}
